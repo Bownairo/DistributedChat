79a5a38a44f009290ef0785caa2f35af18737cff
Eero Kelly <eerokelly@gmail.com>
2017-05-13 19:52:32 -0400

    Add writeup and fix run file

--- run.sh
+++ run.sh
@@ -3,5 +3,7 @@ mkdir -p log
 rm log/*
 for i in $(seq 1 $1)
 do
+	echo "Creating server $i"
 	dotnet run -p Server --server.urls "http://localhost:500$i" --myAddress "localhost:600$i" &> log/server$i.log &
+    sleep 1
 done
